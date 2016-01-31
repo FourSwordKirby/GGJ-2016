c56c0f6e602a72550a2cf6fc4923d2750cc2371d
Stephen Chen <stephen1@andrew.cmu.edu>
2016-01-31 04:43:15 -0500

    Proper animations for Drum and SeeSaw.

--- GGJ-2016/Assets/Scripts/Puzzle/Concrete/SeeSawPuzzle/Weight.cs
+++ GGJ-2016/Assets/Scripts/Puzzle/Concrete/SeeSawPuzzle/Weight.cs
@@ -10,23 +10,36 @@ public class Weight : MonoBehaviour {
 
     private Rigidbody2D body;
     private Text text;
+    private Animator anim;
 
 	// Use this for initialization
 	void Start () {
         body = this.GetComponent<Rigidbody2D>();
         text = this.GetComponentInChildren<Text>();
+        anim = this.GetComponent<Animator>();
+        Debug.Log("Text is " + text);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if(debug)
         {
-            text.gameObject.SetActive(true);
+            //text.enabled = true;
             text.text = "" + body.mass;
         }
         else
         {
-            text.gameObject.SetActive(false);
+            text.text = "";
+            //text.enabled = false;
+        }
+        anim.SetFloat("Speed", Mathf.Abs(body.velocity.x));
+        if(body.velocity.x > 0.2f)
+        {
+            this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+        else if (body.velocity.x < -0.2f)
+        {
+            this.transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
         }
 	}
 
