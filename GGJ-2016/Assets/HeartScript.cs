cf165b4a40716f71ff81d365ef4dfb0176aa349c
Roger Liu <rogliu26@gmail.com>
2016-01-31 13:20:35 -0500

    added hearts to indicate failures

---
+++ GGJ-2016/Assets/HeartScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HeartScript : MonoBehaviour {
+
+    public PuzzleManager parent;
+    public List<GameObject> hearts;
+	
+	// Update is called once per frame
+	void Update () {
+        if (parent != null)
+        {
+            for (int i = 0; i < hearts.Count; i++)
+            {
+                if (i < parent.remainingFailures)
+                    hearts[i].SetActive(true);
+                else
+                    hearts[i].SetActive(false);
+            }
+        }
+	}
+}
